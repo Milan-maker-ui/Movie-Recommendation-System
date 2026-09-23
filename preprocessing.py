9d6a8d84cd977e15be2188de4dc33b9a7b6052e5
milan-ml <arotemilan@gmail.com>
2026-09-23 09:11:31 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,17 +1,13 @@
 import pandas as pd
 
-
 def movie_statistics(movie_data):
     """
     Calculate average movie ratings
     and number of ratings.
     """
-
     ratings_summary = pd.DataFrame(movie_data.groupby("title")["rating"].mean())
-
     ratings_summary["num_of_ratings"] = (movie_data.groupby("title")["rating"].count())
     return ratings_summary
-
 
 def create_user_movie_matrix(movie_data):
     """
@@ -21,7 +17,6 @@ def create_user_movie_matrix(movie_data):
     Columns -> Movies
     Values  -> Ratings
     """
-
     movie_matrix = movie_data.pivot_table(
         index="user_id",
         columns="title",
